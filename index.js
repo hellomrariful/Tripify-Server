931289127e3ab33f4c0be7ee1c9df3a9b97eeba5
Ariful Islam <hello.mrariful@gmail.com>
2023-11-08 23:12:47 +0600

    customaized code

--- index.js
+++ index.js
@@ -10,10 +10,9 @@ app.use(cors())
 app.use(express.json())
 
 // mongoDB
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.c6bvskv.mongodb.net/?retryWrites=true&w=majority`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -24,7 +23,6 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
 
     const serviceCollection = client.db('serviceDB').collection('service');
@@ -44,7 +42,6 @@ async function run() {
       res.send(result)
     })
 
-
     app.get('/dashboard/AddServices/:id', async (req, res) =>{
       const id = req.params.id;
       const query = {_id: new ObjectId(id)};
@@ -52,17 +49,13 @@ async function run() {
       res.send(result)
     })
 
-    // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
 run().catch(console.dir);
-
-
 
 // testing
 app.get('/', (req, res) => {
