8069cf871d4eaf0b46e3c09a25d10a4d8264639b
Ariful Islam <hello.mrariful@gmail.com>
2023-11-08 22:21:42 +0600

    remove await client

--- index.js
+++ index.js
@@ -9,14 +9,9 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
-
 // mongoDB
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.c6bvskv.mongodb.net/?retryWrites=true&w=majority`;
-
-
-
-
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -30,7 +25,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const serviceCollection = client.db('serviceDB').collection('service');
 
@@ -57,22 +52,8 @@ async function run() {
       res.send(result)
     })
 
-
-    // app.post("/dashboard/AddService", async (req, res) => {
-    //   const service = req.body;
-    //   try {
-    //     const result = await serviceCollection.insertOne(service);
-    //     console.log(result);
-    //     res.send(result);
-    //   } catch (error) {
-    //     console.error(error);
-    //     res.status(500).send("Internal Server Error");
-    //   }
-    // });
-
-
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
