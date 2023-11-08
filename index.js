abd2cde52bc6676a948201d338efc201b3a494a0
Ariful Islam <hello.mrariful@gmail.com>
2023-11-08 23:16:26 +0600

    remove all comment code

--- index.js
+++ index.js
@@ -5,13 +5,10 @@ require('dotenv').config();
 const app = express()
 const port = process.env.PORT || 5000
 
-// middleware
 app.use(cors())
 app.use(express.json())
 
-// mongoDB
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.c6bvskv.mongodb.net/?retryWrites=true&w=majority`;
-
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -23,7 +20,6 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // await client.connect();
 
     const serviceCollection = client.db('serviceDB').collection('service');
 
@@ -49,7 +45,6 @@ async function run() {
 
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // await client.close();
   }
 }
 run().catch(console.dir);
